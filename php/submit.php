50e6a4dfaba7e646403b797018aed9c3d17773f2
spoconno <spoconno@oakland.edu>
2012-11-15 16:44:31 -0500

    I have added the wall.php

--- php/submit.php
+++ php/submit.php
@@ -32,46 +32,11 @@ if (!mysql_query($sql,$con))
   {
   die('Error: ' . mysql_error());
   }
-echo "1 record added";
+echo "<br> 1 record added";
 
 mysql_close($con);
 ?> 
 
-<?php echo $_POST["title"];/*
-	$dsn = "mysql:dbname=spoconno";
-	$username = "spoconno";
-	$password = "tacotruck";
 
-	try {
-	$conn = new PDO( $dsn, $username, $password );
-	$conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-	} catch ( PDOException $e ) {
-		echo "Connection failed: " . $e->getMessage();
-	}
-
-	
-	
-	$sql = "INSERT INTO posts (lastName, firstName, title, description, imageURL, price, contactInfo, category)	values ('$_POST[lName]', '$_POST[fName]', '$_POST[title]', '$_POST[description]', '$_POST[img]', '$_POST[price]', '$_POST[email]', '$_POST[select]')";
-	
-	$sql = "SELECT * FROM posts";
-	echo "<ul>";
-	
-	try {
-		$rows = $conn->query( $sql );
-		foreach ( $rows as $row ) {
-			echo "<li>lastName: " . $row["lastName"] . " firstName: " . $row["firstName"] . "</li>";
-			echo "<li>title: " . $row["title"] . " description: " . $row["description"] . "</li>";
-			echo "<li>imageURL: " . $row["imageURL"] . " price: " . $row["price"] . "</li>";
-			echo "<li>contactInfo: " . $row["contactInfo"] . " category: " . $row["category"] . "</li>";
-			echo "<li>postTime: " . $row["postTime"] . " approval: " . $row["approval"] . "</li>";
-			echo "<li>terminateDate: " . $row["terminateDate"] . " postID: " . $row["postID"] . "</li>";
-		}
-	}
-	catch ( PDOException $e ) {
-		echo "Query fialed: " . $e->getMessage();
-	}
-	echo "</ul>";
-	$conn = null;*/
-?>	
 </body>
 </html>
